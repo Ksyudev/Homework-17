0387654976657b4445cfff6c9f0b3465bbfdd75a
Ksyudev <92163392+Ksyudev@users.noreply.github.com>
2021-11-18 09:34:59 +0200

    ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ for (let i=1;i<=100;i++) {
 console.log(`Sum = ${sum}`);*/
 
 let counter = 0;
-for (let a=3;a<=7;a++)
+for (let a=3;a<=1000;a++)
 {let dividers = 0;
     for (let i=1;i<=a;i++)
     {if ((a%i)==0)
